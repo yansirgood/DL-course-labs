675ce49f3f315cc16b5ea033724b1d0aee6bb2fa
yansirgood <119858350+yansirgood@users.noreply.github.com>
2025-01-10 03:25:44 +0800

    feat:完成RNN情感分析

---
+++ RNN/dataset.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+import torch
+
+device = torch.device('cuda:0')
+class NameDataset(Dataset):
+    def __init__(self, file_path, train=True, val=False):
+        self.data = pd.read_csv(file_path, sep='\t')
+        if train:
+            self.data = self.data.sample(frac=0.8, replace=False, random_state=1, axis=0)
+            self.data = self.data.reset_index(drop=True)
+            self.len = self.data.shape[0]
+        elif val:
+            self.data = self.data.sample(frac=0.2, replace=False, random_state=1, axis=0)
+            self.data = self.data.reset_index(drop=True)
+            self.len = self.data.shape[0]
+        self.phrase, self.sentiment = self.data['Phrase'], self.data['Sentiment']
+    def __getitem__(self, index):
+        # 根据数据索引获取样本
+       return self.phrase[index], self.sentiment[index]
+
+    def __len__(self):
+        # 返回数据长度
+        return self.len
+
+
+def phrase2list(phrase):
+    if isinstance(phrase, str):
+        arr = [ord(c) for c in phrase]
+    else:
+        arr = [ord(c) for c in str(phrase)]
+    return arr, len(arr)
+
+def make_tensors(phrase, sentiment):
+    sequences_and_lengths = [phrase2list(phrase) for phrase in phrase]
+    phrase_sequences = [sl[0] for sl in sequences_and_lengths if sl[1]>0]
+    seq_lengths = torch.LongTensor([sl[1] for sl in sequences_and_lengths if sl[1]>0])
+    sentiment = sentiment.long()
+
+    seq_tensor = torch.zeros(len(phrase_sequences), seq_lengths.max()).long()
+    for idx, (seq, seq_len) in enumerate(zip(phrase_sequences, seq_lengths), 0):
+        seq_tensor[idx, :seq_len] = torch.LongTensor(seq)
+    seq_lengths, prem_idx = seq_lengths.sort(dim=0, descending=True)
+    seq_tensor = seq_tensor[prem_idx]
+    sentiment = sentiment[prem_idx]
+    return seq_tensor.to(device), seq_lengths.to(device), sentiment.to(device)
+
+
+def make_tensors1(phrase):
+    sequences_and_lengths = [phrase2list(phrase) for phrase in phrase]
+    phrase_sequences = [sl[0] for sl in sequences_and_lengths if sl[1]>0]
+    seq_lengths = torch.LongTensor([sl[1] for sl in sequences_and_lengths if sl[1]>0])
+
+    seq_tensor = torch.zeros(len(phrase_sequences), seq_lengths.max()).long()
+    for idx, (seq, seq_len) in enumerate(zip(phrase_sequences, seq_lengths), 0):
+        seq_tensor[idx, :seq_len] = torch.LongTensor(seq)
+    seq_lengths, prem_idx = seq_lengths.sort(dim=0, descending=True)
+    seq_tensor = seq_tensor[prem_idx]
+    _, index = prem_idx.sort(descending=False)
+    return seq_tensor.to(device), seq_lengths.to(device), index
---
+++ RNN/train.py
@@ -0,0 +1,58 @@
+import torch
+from dataset import NameDataset,phrase2list,make_tensors
+from RNN import RNNClassifier
+from torch.utils.data import Dataset, DataLoader
+from config import device,NUM_CHARS,HIDDEN_SIZE,NUM_LAYERS,NUM_EPOCHS,BATCH_SIZE,train_set,train_loader,NUM_CLASS,patience,val_loader,val_set,droup_out
+
+
+def train_and_validate(model, criterion, optimizer, train_loader, val_loader, device):
+    model.train()
+    total_loss = 0
+    for phrase, sentiment in train_loader:
+        inputs, seq_lengths, target = make_tensors(phrase, sentiment)
+        inputs, seq_lengths, target = inputs.to(device), seq_lengths.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(inputs, seq_lengths)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+
+    model.eval()
+    val_loss = 0
+    with torch.no_grad():
+        for phrase, sentiment in val_loader:
+            inputs, seq_lengths, target = make_tensors(phrase, sentiment)
+            inputs, seq_lengths, target = inputs.to(device), seq_lengths.to(device), target.to(device)
+            output = model(inputs, seq_lengths)
+            loss = criterion(output, target)
+            val_loss += loss.item()
+    return total_loss / len(train_loader), val_loss / len(val_loader)
+
+def return_loss_list():
+    return loss_list
+def return_classifier():
+    classifier = torch.load('sentimentAnalyst.pkl')
+    classifier.to(device)
+    return classifier
+if __name__ == '__main__':
+    classifier = RNNClassifier(NUM_CHARS, HIDDEN_SIZE, NUM_CLASS, droup_out,NUM_LAYERS).to(device)
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(classifier.parameters(), lr=0.001)
+    loss_list=[]
+    patience_counter=0
+    for epoch in range(1, NUM_EPOCHS + 1):
+        train_loss, val_loss = train_and_validate(classifier, criterion, optimizer, train_loader, val_loader, device)
+        print(f'Epoch {epoch}, Train Loss: {train_loss}, Val Loss: {val_loss}')
+        loss_list.append(val_loss)
+        if val_loss <= min(loss_list):
+            torch.save(classifier, 'sentimentAnalyst.pkl')
+            print('Save Model!')
+        else:
+            patience_counter+=1
+            if patience_counter >=patience:
+                print('早退')
+                break
+            return_loss_list()
+    return_classifier()
+
